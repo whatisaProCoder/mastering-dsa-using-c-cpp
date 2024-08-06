3a32dff40ab9e11539568c0ef9729dde9fd62ee0
whatisaProCoder <debnathpritam2005@gmail.com>
2024-08-06 21:51:05 +0530

    New Commit

--- Section 08 - Strings/Operations on String I/main.c
+++ Section 08 - Strings/Operations on String I/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-///Finding Length of a String
+/// Finding Length of a String
 
-int main()
-{
-    char s[]="welcome";
+int main() {
+    char s[] = "welcome";
     int i;
 
-    for(i=0;s[i]!='\0';i++){}
+    for (i = 0; s[i] != '\0'; i++) {
+    }
 
-    printf("Length is %d",i);
+    printf("Length is %d", i);
 
     return 0;
 }
