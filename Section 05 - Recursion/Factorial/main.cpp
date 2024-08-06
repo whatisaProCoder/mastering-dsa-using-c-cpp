3be26a3d50b46f9ba8d223bc7a02c66bc0f230e6
whatisaProCoder <debnathpritam2005@gmail.com>
2024-08-06 22:22:02 +0530

    New Commit

--- Section 05 - Recursion/Factorial/main.cpp
+++ Section 05 - Recursion/Factorial/main.cpp
@@ -2,25 +2,22 @@
 
 using namespace std;
 
-int fact(int n)
-{
-    if(n==0)
+int fact(int n) {
+    if (n == 0)
         return 1;
-    return fact(n-1)*n;
+    return fact(n - 1) * n;
 }
 
-int ifact(int n)
-{
-    int i, f=1;
-    for(i=1; i<=n; i++)
-        f=f*i;
+int ifact(int n) {
+    int i, f = 1;
+    for (i = 1; i <= n; i++)
+        f = f * i;
     return f;
 }
 
-int main()
-{
+int main() {
     int r;
-    r=ifact(5);
-    cout<<r;
+    r = ifact(5);
+    cout << r;
     return 0;
 }
